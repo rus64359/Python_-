162500f9a1380d473ff0663cffdeb5ff4dd5189a
rus64359 <rus64359@gmail.com>
2026-08-22 17:37:08 +0900

    Исправленны ошибки

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,6 +2,6 @@ from user import User
 
 my_user = User("Vyacheclav", "Bezgin")
 
-my_user.First()
-my_user.Last()
-my_user.name()
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -1,15 +1,13 @@
 class User:
-
     def __init__(self, first_name, last_name):
-        print("")
-        self.first = first_name
-        self.last = last_name
+        self.first_name = first_name
+        self.last_name = last_name
 
-    def First(self):
-        print("Моё имя", self.first)
+    def print_first_name(self):
+        print("First Name:", self.first_name)
 
-    def Last(self):
-        print("Моя фамилия", self.last)
+    def print_last_name(self):
+        print("Last Name:", self.last_name)
 
-    def name(self):
-        print("Моё имя и фамилия:", self.first, self.last)
+    def print_full_name(self):
+        print("Full Name:", self.first_name, self.last_name)
